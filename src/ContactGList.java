edc22358b266fe0eb7e2ce9b22c6e3ed8033c3e6
达斯特兰斯 <kk580kk@gmail.com>
2013-08-23 09:41:20 +0800

    测试push到那个branch

--- src/ContactGList.java
+++ src/ContactGList.java
@@ -165,8 +165,8 @@ public class ContactGList extends JPanel implements ActionListener,
         // Add Contact List
         addContactListToWorkspace();
 
-        ContactGGroup contactGGroup = new ContactGGroup("unfiledGroup");
-        this.addContactGroup(contactGGroup);
+        ContactGGroup contactGGroup1 = new ContactGGroup("unfiledGroup");
+        this.addContactGroup(contactGGroup1);
 
 
         this.setVisible(true);
